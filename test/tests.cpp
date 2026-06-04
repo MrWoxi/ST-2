c0d1da9e1bd616b4abcf6c6de4217641925c9ffe
MrWoxi <klimovichvita2005@gmail.com>
2026-06-04 12:53:02 +0300

    Add more tests and result files

--- test/tests.cpp
+++ test/tests.cpp
@@ -9,74 +9,119 @@
 const double EPS = 1e-6;
 const double PI = 3.14159265358979;
 
-TEST(Circle, constructor) {
+TEST(Circle, constructorSetsRadius) {
   Circle c(5.0);
   EXPECT_NEAR(c.getRadius(), 5.0, EPS);
+}
+TEST(Circle, constructorSetsFerence) {
+  Circle c(5.0);
   EXPECT_NEAR(c.getFerence(), 2.0 * PI * 5.0, EPS);
+}
+TEST(Circle, constructorSetsArea) {
+  Circle c(5.0);
   EXPECT_NEAR(c.getArea(), PI * 25.0, EPS);
 }
-
-TEST(Circle, constructorThrows) {
+TEST(Circle, constructorThrowsZero) {
   EXPECT_THROW(Circle(0.0), std::invalid_argument);
-  EXPECT_THROW(Circle(-1.0), std::invalid_argument);
 }
-
-TEST(Circle, setRadius) {
+TEST(Circle, constructorThrowsNegative) {
+  EXPECT_THROW(Circle(-3.0), std::invalid_argument);
+}
+TEST(Circle, setRadiusUpdatesAll) {
   Circle c(1.0);
   c.setRadius(4.0);
   EXPECT_NEAR(c.getRadius(), 4.0, EPS);
   EXPECT_NEAR(c.getFerence(), 2.0 * PI * 4.0, EPS);
+  EXPECT_NEAR(c.getArea(), PI * 16.0, EPS);
+}
+TEST(Circle, setRadiusThrowsZero) {
+  Circle c(1.0);
   EXPECT_THROW(c.setRadius(0.0), std::invalid_argument);
 }
-
-TEST(Circle, setFerence) {
+TEST(Circle, setRadiusThrowsNegative) {
+  Circle c(1.0);
+  EXPECT_THROW(c.setRadius(-1.0), std::invalid_argument);
+}
+TEST(Circle, setFerenceUpdatesAll) {
   Circle c(1.0);
   c.setFerence(2.0 * PI * 7.0);
   EXPECT_NEAR(c.getRadius(), 7.0, EPS);
-  EXPECT_THROW(c.setFerence(-10.0), std::invalid_argument);
+  EXPECT_NEAR(c.getArea(), PI * 49.0, EPS);
 }
-
-TEST(Circle, setArea) {
+TEST(Circle, setFerenceThrows) {
+  Circle c(1.0);
+  EXPECT_THROW(c.setFerence(0.0), std::invalid_argument);
+  EXPECT_THROW(c.setFerence(-5.0), std::invalid_argument);
+}
+TEST(Circle, setAreaUpdatesAll) {
   Circle c(1.0);
   c.setArea(PI * 9.0);
   EXPECT_NEAR(c.getRadius(), 3.0, EPS);
+  EXPECT_NEAR(c.getFerence(), 2.0 * PI * 3.0, EPS);
+}
+TEST(Circle, setAreaThrows) {
+  Circle c(1.0);
   EXPECT_THROW(c.setArea(0.0), std::invalid_argument);
+  EXPECT_THROW(c.setArea(-1.0), std::invalid_argument);
+}
+TEST(Circle, chainedUpdates) {
+  Circle c(2.0);
+  c.setArea(PI * 4.0);
+  c.setFerence(2.0 * PI * 6.0);
+  EXPECT_NEAR(c.getRadius(), 6.0, EPS);
 }
 
 TEST(EarthRope, gapIsConstant) {
-  double gap1 = getEarthRopeGap(6378.1, 1.0);
-  double gap2 = getEarthRopeGap(1000.0, 1.0);
-  EXPECT_NEAR(gap1, gap2, EPS);
+  double g1 = getEarthRopeGap(6378.1, 1.0);
+  double g2 = getEarthRopeGap(1000.0, 1.0);
+  EXPECT_NEAR(g1, g2, EPS);
 }
-
-TEST(EarthRope, formula) {
+TEST(EarthRope, oneMeterFormula) {
   EXPECT_NEAR(getEarthRopeGap(6378.1, 1.0), 1.0 / (2.0 * PI), EPS);
+}
+TEST(EarthRope, twoMeterFormula) {
   EXPECT_NEAR(getEarthRopeGap(6378.1, 2.0), 2.0 / (2.0 * PI), EPS);
 }
-
-TEST(EarthRope, throws) {
+TEST(EarthRope, defaultArgs) {
+  EXPECT_NEAR(getEarthRopeGap(), 1.0 / (2.0 * PI), EPS);
+}
+TEST(EarthRope, throwsZeroRadius) {
   EXPECT_THROW(getEarthRopeGap(0.0, 1.0), std::invalid_argument);
+}
+TEST(EarthRope, throwsNegativeRope) {
   EXPECT_THROW(getEarthRopeGap(6378.1, -1.0), std::invalid_argument);
 }
 
-TEST(Pool, defaults) {
+TEST(Pool, totalEqualsParts) {
   PoolCosts p = calculatePoolCosts();
   EXPECT_NEAR(p.totalCost, p.concreteCost + p.fenceCost, EPS);
 }
-
-TEST(Pool, custom) {
-  PoolCosts p = calculatePoolCosts(2.0, 1.0, 500.0, 1000.0);
-  double ec = (PI * 9.0 - PI * 4.0) * 500.0;
-  double ef = 2.0 * PI * 3.0 * 1000.0;
-  EXPECT_NEAR(p.concreteCost, ec, EPS);
-  EXPECT_NEAR(p.fenceCost, ef, EPS);
+TEST(Pool, concreteCost) {
+  PoolCosts p = calculatePoolCosts(3.0, 1.0, 1000.0, 2000.0);
+  double area = PI * 16.0 - PI * 9.0;
+  EXPECT_NEAR(p.concreteCost, area * 1000.0, EPS);
 }
-
-TEST(Pool, throws) {
-  EXPECT_THROW(calculatePoolCosts(0.0, 1.0, 1000.0, 2000.0),
+TEST(Pool, fenceCost) {
+  PoolCosts p = calculatePoolCosts(3.0, 1.0, 1000.0, 2000.0);
+  EXPECT_NEAR(p.fenceCost, 2.0 * PI * 4.0 * 2000.0, EPS);
+}
+TEST(Pool, customValues) {
+  PoolCosts p = calculatePoolCosts(2.0, 1.0, 500.0, 1000.0);
+  EXPECT_NEAR(p.concreteCost, (PI * 9.0 - PI * 4.0) * 500.0, EPS);
+  EXPECT_NEAR(p.fenceCost, 2.0 * PI * 3.0 * 1000.0, EPS);
+}
+TEST(Pool, throwsZeroRadius) {
+  EXPECT_THROW(
+    calculatePoolCosts(0.0, 1.0, 1000.0, 2000.0),
     std::invalid_argument);
-  EXPECT_THROW(calculatePoolCosts(3.0, -1.0, 1000.0, 2000.0),
+}
+TEST(Pool, throwsNegativeTrack) {
+  EXPECT_THROW(
+    calculatePoolCosts(3.0, -1.0, 1000.0, 2000.0),
     std::invalid_argument);
-  EXPECT_THROW(calculatePoolCosts(3.0, 1.0, -100.0, 2000.0),
+}
+TEST(Pool, throwsNegativeConcrete) {
+  EXPECT_THROW(
+    calculatePoolCosts(3.0, 1.0, -100.0, 2000.0),
     std::invalid_argument);
 }
